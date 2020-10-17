9b56df92222a8cd3bc5e92cf40301370bc6f962c
Swathi Mukkamala <swathikmukkamala@gmail.com>
2020-10-17 15:05:45 -0400

    working on help page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CardGame.Controllers
             return View();
         }
 
+        public IActionResult Help()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,13 +2,13 @@
 SET Score = 145
 Where Id='fcf0a376-c7db-416a-a2d9-960049a65c93'
 
-DELETE FROM PurchasedDecks WHERE UserId='fcf0a376-c7db-416a-a2d9-960049a65c93'*/
+DELETE FROM PurchasedDeck WHERE UserId='fcf0a376-c7db-416a-a2d9-960049a65c93'*/
 
 /*UPDATE AspNetUsers
 SET Score = 145
 Where Id='ea10deff-f6e0-4c5d-8c90-af4a766aa7e3'
 
-DELETE FROM PurchasedDecks WHERE UserId='ea10deff-f6e0-4c5d-8c90-af4a766aa7e3'*/
+DELETE FROM PurchasedDeck WHERE UserId='ea10deff-f6e0-4c5d-8c90-af4a766aa7e3'*/
 
 
 SELECT * FROM PurchasedDeck
